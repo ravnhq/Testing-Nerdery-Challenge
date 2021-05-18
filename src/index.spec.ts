57a6342ed9297b540606acbbfde671b7739f7411
Gerardo Estrada <gestrada@Gerardos-MacBook-Pro.local>
2021-05-18 16:17:51 -0600

    add imports to test file

--- src/index.spec.ts
+++ src/index.spec.ts
@@ -1,3 +1,12 @@
+import {
+  isInteger,
+  toLowerCase,
+  removeDuplicatesFromArray,
+  createRandomProduct,
+  getStarWarsPlanets,
+  createProduct 
+} from './index';
+
 test('Testing time!', () => {
   console.log('welcome');
 });
