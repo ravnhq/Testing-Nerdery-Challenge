d571dc679fe49c6146d087ccb761bf9e6c22ba36
Kevin Cotrina <kcotrinam92@gmail.com>
2021-09-14 15:47:46 -0500

    Add tests for isInteger function

--- src/index.spec.ts
+++ src/index.spec.ts
@@ -4,9 +4,31 @@ import {
   removeDuplicatesFromArray,
   createRandomProduct,
   getStarWarsPlanets,
-  createProduct 
+  createProduct,
 } from './index';
 
-test('Testing time!', () => {
-  console.log('welcome');
+describe('Test isInteger function', () => {
+  it('return true if we pass a number', () => {
+    expect(isInteger(1)).toBe(true);
+  });
+
+  it('return true if we pass a negative number', () => {
+    expect(isInteger(-1)).toBe(true);
+  });
+
+  it('return false if we pass a decimal number', () => {
+    expect(isInteger(-10.4)).toBe(false);
+  });
+
+  it('return false if we pass a string', () => {
+    expect(isInteger('1')).toBe(false);
+  });
+
+  it('return false if we pass a NaN', () => {
+    expect(isInteger(NaN)).toBe(false);
+  });
+
+  it('return false if we pass a null', () => {
+    expect(isInteger(null)).toBe(false);
+  });
 });
