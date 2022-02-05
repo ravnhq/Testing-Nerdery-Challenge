78f0ccb20c3802d5bf4c02e4b75136f72b7d480a
Xrls9 <jagox28@outlook.com>
2022-02-04 19:58:20 -0500

    feat: added tests, lines coverage at 97.3%

--- src/index.spec.ts
+++ src/index.spec.ts
@@ -1,4 +1,6 @@
-import cases from 'jest-in-case'
+import cases from 'jest-in-case';
+import fetch from 'node-fetch';
+import { mocked } from 'ts-jest/utils';
 import {
   isInteger,
   toLowerCase,
@@ -8,76 +10,173 @@ import {
   createProduct,
 } from './index';
 
+jest.mock('node-fetch', () => jest.fn());
 
-describe('isInteger',()=>{
-  cases('should return',
-  (opts) => {
-    expect(isInteger(opts.value)).toBe(opts.result)
-  },
-  [
-    {name:'true evaluating a number', value:123, result:true},
-    {name:'true evaluating a negative number', value:-123, result:true},
-    {name:'false evaluating a string', value:'1', result:false},
-    {name:'false evaluating a float', value:1.25, result:false},
-  ])
-})
+describe('isInteger', () => {
+  cases(
+    'should return',
+    (opts) => {
+      expect(isInteger(opts.actual)).toBe(opts.expected);
+    },
+    [
+      { name: 'true evaluating a number', actual: 123, expected: true },
+      {
+        name: 'true evaluating a negative number',
+        actual: -123,
+        expected: true,
+      },
+      { name: 'false evaluating a string', actual: '1', expected: false },
+      { name: 'false evaluating a float', actual: 1.25, expected: false },
+    ],
+  );
+});
 
-describe('toLowerCase',()=>{
+describe('toLowerCase', () => {
+  cases(
+    `should return`,
+    (opts) => {
+      expect(toLowerCase(opts.actual)).toEqual(opts.expected);
+    },
+    [
+      {
+        name: 'prueba when PrueBa is given',
+        actual: 'PrueBa',
+        expected: 'prueba',
+      },
+      {
+        name: 'the same string when a lowercase string is given',
+        actual: 'lowercase',
+        expected: 'lowercase',
+      },
+      {
+        name: 'return error when passing null or an empty string',
+        actual: null,
+        expected: 'Please provide a string',
+      },
+    ],
+  );
+});
 
-  cases(`should return`,
-  (opts) => {
-    expect(toLowerCase(opts.value)).toEqual(opts.result)
-  },
-  [
-    {name:'prueba when PrueBa is given', value:'PrueBa', result:'prueba'},
-    {name:'the same string when a lowercase string is given', value:'lowercase', result:'lowercase'},
-    {name:'return error when passing null or an empty string', value:null, result:'Please provide a string'},
-  ])
-})
+describe('removeDuplicatesFromArray', () => {
+  cases(
+    `should return`,
+    (opts) => {
+      expect(removeDuplicatesFromArray(opts.actual)).toStrictEqual(
+        opts.expected,
+      );
+    },
+    [
+      {
+        name: '[1,2,3] when [1,2,2,3,1] is evaluated',
+        actual: [1, 2, 2, 3, 1],
+        expected: [1, 2, 3],
+      },
+      {
+        name: `['1','2','3'] when ['1','2','2','3','1'] is evaluated`,
+        actual: ['1', '2', '2', '3', '1'],
+        expected: ['1', '2', '3'],
+      },
+      {
+        name: 'the same array when the array only has one element',
+        actual: ['one'],
+        expected: ['one'],
+      },
+    ],
+  );
 
-describe('removeDuplicatesFromArray',()=>{
+  cases(
+    'should throw an error',
+    (opts) => {
+      expect(() => removeDuplicatesFromArray(opts.actual as any)).toThrowError(
+        opts.expected,
+      );
+    },
+    [
+      {
+        name: 'when there is no an array given',
+        actual: {},
+        expected: 'please provide an array of numbers or strings',
+      },
+    ],
+  );
+});
 
-  cases(`should return`,
-  (opts) => {
-    expect(removeDuplicatesFromArray(opts.value)).toEqual(opts.result)
-  },
-  [
-    {name:'[1,2,3] when [1,2,2,3,1] is evaluated', value:[1,2,2,3,1], result:[1,2,3]},
-    {name:`['1','2','3'] when ['1','2','2','3','1'] is evaluated`, value:['1','2','2','3','1'], result:['1','2','3']},
-    {name:'the same array when the array only has one element', value:['one'], result:['one']},
-  ])
+describe('createRandomProduct', () => {
+  it('should return a random product when giving a creator role email', () => {
+    const creatorEmail = 'clark@kent.com';
+    const actual = createRandomProduct(creatorEmail);
+    expect(actual).not.toBeNull();
+  });
 
-  cases('should throw an error',(opts)=>{
-    expect(()=>removeDuplicatesFromArray(opts.value as any))
-      .toThrowError(opts.result)
-  },
-  [
-    {name: 'when there is no an array given', value:{}, result:'please provide an array of numbers or strings'}
-  ])
-})
+  it('should throw an error when user does not have creator role', () => {
+    const noCreatorUser = 'diana@themyscira.com';
+    expect(() => createRandomProduct(noCreatorUser)).toThrowError(
+      'You are not allowed to create products',
+    );
+  });
 
-describe('createRandomProduct', ()=>{
+  it('should throw an error when passing an invalid email', () => {
+    const invalidEmail = 'invalidemail@fake.com';
+    expect(() =>
+      createRandomProduct(invalidEmail),
+    ).toThrowErrorMatchingInlineSnapshot(
+      `"Cannot read properties of undefined (reading 'role')"`,
+    );
+  });
+});
 
-  it('should return a random product when giving a creator role email',()=>{
-    const creatorEmail = 'clark@kent.com'
-    expect(()=> createRandomProduct(creatorEmail)).not.toBeNull()
-  })
+describe('createProduct', () => {
+  const testProduct = {
+    name: 'motorcycle',
+    description: 'arch motorcycle',
+    price: 1000,
+    tags: ['at least one'],
+  };
 
-  it('should throw an error when user does not have creator role', ()=>{
-    const noCreatorUser = 'diana@themyscira.com'
-    expect(()=> createRandomProduct(noCreatorUser))
-      .toThrowErrorMatchingInlineSnapshot(`"You are not allowed to create products"`)
-  })
+  it('should return a valid product', () => {
+    const actual = createProduct(testProduct);
 
-  it('should throw an error when passing an invalid email', ()=>{
-    const invalidEmail = 'invalidemail@fake.com'
-    expect(()=> createRandomProduct(invalidEmail))
-      .toThrowErrorMatchingInlineSnapshot(`"Cannot read properties of undefined (reading 'role')"`)
-  })
+    expect(actual).toEqual({
+      id: expect.any(Number),
+      ...testProduct,
+    });
+  });
 
-})
+  it('should throw an error whit a bad forming object', () => {
+    const invalidProduct = testProduct;
+    invalidProduct.tags = [];
 
+    expect(() => createProduct(invalidProduct)).toThrowError();
+  });
+});
 
+describe('getStarWarsPlanets', () => {
+  it('should throw an error if something goes wrong', () => {
+    mocked(fetch).mockImplementation((): Promise<any> => {
+      return Promise.resolve({
+        json: () => Promise.reject()
+      });
+    });
 
+    expect(
+      async () => await getStarWarsPlanets()
+    ).rejects.toThrow();
 
+    expect(fetch).toHaveBeenCalledTimes(1);
+    expect(fetch).toHaveBeenCalledWith('https://swapi.dev/api/planets');
+  });
 
+  it('should fetch data from api', () => {
+    mocked(fetch).mockImplementation((): Promise<any> => {
+      return Promise.resolve({
+        json: Promise.resolve({}),
+      });
+    });
+
+    const response = async () => await getStarWarsPlanets();
+
+    expect(fetch).toHaveBeenCalledTimes(1);
+    expect(fetch).toHaveBeenCalledWith('https://swapi.dev/api/planets');
+    expect(Array.isArray(response)).toEqual(false);
+  });
+});
