8792097b83509d98005d87bf586c44a75de2eda5
Kevin DC <kevindelcastillo@ravn.co>
2023-02-15 10:13:06 -0500

    fix: Fix small formatting issues

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,17 +1,16 @@
 module.exports = {
-    env: {
-      node: true,
-    },
-    extends: ['eslint:recommended', 'prettier'],
-    plugins: ['prettier'],
-    rules: {
-      'prettier/prettier': [
-        'error',
-        {
-          singleQuote: true,
-          semi: true,
-        },
-      ],
-    },
-  };
-  
+  env: {
+    node: true,
+  },
+  extends: ['eslint:recommended', 'prettier'],
+  plugins: ['prettier'],
+  rules: {
+    'prettier/prettier': [
+      'error',
+      {
+        singleQuote: true,
+        semi: true,
+      },
+    ],
+  },
+};
